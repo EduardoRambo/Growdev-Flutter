769ba9b370f951935581b41c11be9f801b6de65e
EduardoRambo <eduardorambolima@gmail.com>
2020-06-30 16:36:18 -0300

    Exercício MyFirstApp

---
+++ MyFirstAppFlutter/lib/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _calc = 1;
+
+  void _clicarLongPress() {
+    setState(() {
+      _calc = 1;
+    });
+  }
+
+  void _clicarOnTap() {
+    setState(() {
+      _calc = _calc++ * 2; 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("My first app")),
+      body: GestureDetector(
+        child: Center(
+          child: Text(
+            'calc $_calc',
+            style: TextStyle(
+              fontSize: 28,
+            ),
+          ),
+        ),
+        onTap: _clicarOnTap,
+        onLongPress: _clicarLongPress,
+      ),
+    );
+  }
+}
---
+++ MyFirstAppFlutter/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/home_page.dart';
+
+void main() {
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage()
+    );
+  }
+}
